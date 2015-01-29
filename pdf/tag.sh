44211a4b30dd0267431552f17b015c8acef77f9b
Tim Menzies <tim@menzies.us>
2015-01-29 00:43:07 -0500

    stuff

--- pdf/tag.sh
+++ pdf/tag.sh
@@ -1,31 +1,33 @@
-set -x
+
 
 rm -rf all.html clean.txt abstracts.txt words.txt frequencies.txt go-file.txt commonwords.txt
 
-echo $*
 
-for f in `find . -name "$*"`
-do  
-    echo $f
-    pdftohtml -stdout $f >> all.html
-done
-lynx -dump all.html >> all.txt
-perl -lpe s/[^[:print:]]+//g all.txt >> clean.txt
-sed -n '/ABSTRACT/,/INTRODUCTION/p' < clean.txt | \
-grep -v -w INTRODUCTION > abstracts.txt
-curl -o stopwords.txt http://skipperkongen.dk/files/english-stopwords-short.txt
-grep -v -w ABSTRACT < abstracts.txt | \
-sed 's/[^a-zA-Z]/ /g' | \
-tr '[:upper:]' '[:lower:]' | \
+Tmp=/tmp/$USER/cloud
+mkdir -p $Tmp
+rm -rf $Tmp/*
+
+for f in $*
+do
+    echo $f 
+    pdftotext $f $Tmp/$f.txt
+done 
+
+cat $Tmp/*  |
+perl -lpe s/[^[:print:]]+//g |
+sed 's/[^a-zA-Z]/ /g' | 
+tr '[:upper:]' '[:lower:]' | 
 tr ' ' '
-' | \
-sed '/^$/d' | \
-sed '/^[a-z]$/d' | \
-grep -v -w -f stopwords.txt | \
-sort > words.txt
+' | 
+sed '/^$/d' | 
+sed '/^[a-z]$/d' | 
+grep -v -w -f stopwords.txt | 
+sort |
+uniq -c  | 
+sort -r -n > $Tmp/frequencies.txt
 
-uniq -c < words.txt | sort -r -n > frequencies.txt
 
-head -n500 < frequencies.txt | cut -f3 -d' ' > go-file.txt
+head -n50 < $Tmp/frequencies.txt | gawk '{print $2": " $1}' |
+xclip -selection c
 
-cat words.txt | grep -w -f go-file.txt > commonwords.txt
+#cat words.txt | grep -w -f go-file.txt > commonwords.txt
